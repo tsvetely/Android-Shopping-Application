5abd947c00923bd4845e14f1775c606ba431df38
tevetely <cvetygetova@yahoo.com>
2018-08-14 16:23:04 +0300

    packages
    Added packages

--- ShoppingApp/app/src/main/java/com/getova/shoppingapp/views/MainActivity.java
+++ ShoppingApp/app/src/main/java/com/getova/shoppingapp/views/MainActivity.java
@@ -1,7 +1,9 @@
-package com.getova.shoppingapp;
+package com.getova.shoppingapp.views;
 
 import android.app.Activity;
 import android.os.Bundle;
+
+import com.getova.shoppingapp.R;
 
 public class MainActivity extends Activity {
 
